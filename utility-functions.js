0ccdc8020e6683cc4c287367cfed7ac0824a2603
Xasbus <davidbilliot0828.gmail.com>
2024-12-15 10:42:21 -0600

    commented out old js files

--- utility-functions.js
+++ utility-functions.js
@@ -1,29 +1,29 @@
-function display(message) {
-    document.write(message);
-}
+// function display(message) {
+//     document.write(message);
+// }
 
-function output(message) {
-    window.outputTag.innerHTML += message;
-}
+// function output(message) {
+//     window.outputTag.innerHTML += message;
+// }
 
-function ask(message) {
-    const userText = prompt(message);
-    const userNumber = Number(userText);
-    const isNotNumber = isNaN(userNumber);
-    if (isNotNumber) {
-        return userText;
-    } else {
-        return userNumber;
-    }
-}
+// function ask(message) {
+//     const userText = prompt(message);
+//     const userNumber = Number(userText);
+//     const isNotNumber = isNaN(userNumber);
+//     if (isNotNumber) {
+//         return userText;
+//     } else {
+//         return userNumber;
+//     }
+// }
 
-function cardConverter(cardObject) {
-    let cardString = `<div class="card">
-  <img class="card-img-top" src="${cardObject.src}">
-  <div class="card-body">
-  <h3 class="card-title">${cardObject.title}</h3>
-  ${cardObject.summary}
-  </div>
-  </div>`
-    return cardString;
-}
+// function cardConverter(cardObject) {
+//     let cardString = `<div class="card">
+//   <img class="card-img-top" src="${cardObject.src}">
+//   <div class="card-body">
+//   <h3 class="card-title">${cardObject.title}</h3>
+//   ${cardObject.summary}
+//   </div>
+//   </div>`
+//     return cardString;
+// }
