c843107ffe2735617673636371ef3809e6c2fbf7
Xasbus <davidbilliot0828.gmail.com>
2025-01-15 22:34:06 -0600

    React Working

---
+++ src/index.babel.js
@@ -0,0 +1,4 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+const root = createRoot(window.bodyTag);
+root.render(/*#__PURE__*/React.createElement("main", null));
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+const root = createRoot(window.bodyTag);
+
+root.render(
+  <main>
+    <h1>CHANNGING</h1>
+  </main>
+);
