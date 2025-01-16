13625e26f671784152195c2568f819c1b2cf2f69
Xasbus <davidbilliot0828.gmail.com>
2025-01-16 15:46:04 -0600

    update

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -1,8 +1,12 @@
 import React from "react";
+import { NavBar } from "./NavBar";
 
 export function Home() {
   return (
     <>
+      <header>
+        <NavBar />
+      </header>
       <main>BROWSER ROUTER IS SET UP.</main>
     </>
   );
