9e716140a5469ec164ca7aadea496c40627d62fa
Xasbus <davidbilliot0828.gmail.com>
2025-01-15 23:14:55 -0600

    BrowserRouter is working

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router";
+import { Home } from "./Views/Home";
 
 const root = createRoot(window.bodyTag);
 
 root.render(
-  <main>
-    <h1>CHANNGING</h1>
-  </main>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  </BrowserRouter>
 );
