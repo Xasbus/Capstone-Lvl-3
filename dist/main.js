8b6a9f275947b786da53d07553911b5c8735ea1e
Xasbus <davidbilliot0828.gmail.com>
2025-01-30 22:58:15 -0600

    Fixed github loading

--- dist/main.js
+++ dist/main.js
@@ -2549,7 +2549,7 @@ __webpack_require__.r(__webpack_exports__);
 function NavBar() {
   const domain = window.location.hostname;
   let rootPath = "";
-  if (domain === "xasbus.github.io") rootPath = "";
+  if (domain === "xasbus.github.io") rootPath = "/Capstone-Lvl-3";
   console.log(`The domain is ${domain}`);
   return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement((react__WEBPACK_IMPORTED_MODULE_0___default().Fragment), null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("nav", {
     className: "nav nav-pills"
--- src/Views/NavBar.js
+++ src/Views/NavBar.js
@@ -4,7 +4,7 @@ import { Link, NavLink } from "react-router";
 export function NavBar() {
   const domain = window.location.hostname;
   let rootPath = "";
-  if (domain === "xasbus.github.io") rootPath = "";
+  if (domain === "xasbus.github.io") rootPath = "/Capstone-Lvl-3";
   console.log(`The domain is ${domain}`);
   return (
     <>
